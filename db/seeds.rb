f393c9c6143121abcb14af976e1a3981f0e871ed
emotionalproof <chaserabenn@gmail.com>
2020-07-25 00:30:15 -0400

    seed data for activiies and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# .all.sample.id
+
+
+require 'faker'
+
+# User.destroy_all
+
+Activity.create(name: "Breathe")
+# Activity.create(name: "Create")
+# Activity.create(name: "Disapear")
+Activity.create(name: "Learn")
+# Activity.create(name: "Move")
+Activity.create(name: "Play")
+# Activity.create(name: "Smile")
+Activity.create(name: "Walk")
+
+# User.create({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: "user1", password: "password", password_confirmation: "password"})
+# User.create({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: "user2", password: "password", password_confirmation: "password"})
+# User.create({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: "user3", password: "password", password_confirmation: "password"})
+# User.create({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: "user4", password: "password", password_confirmation: "password"})
+
